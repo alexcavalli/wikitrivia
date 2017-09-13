fabb93f6a15dcbf127189693b760066c61b75cd6
Alex Cavalli <alexpcavalli@gmail.com>
2017-09-12 21:25:49 -0500

    Remove player name setting from quiz creation

--- web/controllers/quiz_controller.ex
+++ web/controllers/quiz_controller.ex
@@ -11,7 +11,7 @@ defmodule Wikitrivia.QuizController do
     render conn, "new.html"
   end
 
-  def create(conn, %{"quiz" => %{"num_players" => num_players, "player_name" => player_name}}) do
+  def create(conn, %{"quiz" => %{"num_players" => num_players}}) do
     %{id: quiz_id, question_ids: question_ids} = Wikitrivia.Quiz.generate
     Wikitrivia.GameRegistry.create(quiz_id, num_players, question_ids)
     conn |> redirect(to: quiz_path(conn, :show, quiz_id))
