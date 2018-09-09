c43af2c544965710d27047f21d2e6986c4b83504
Dennis Heckman <denheck@gmail.com>
2018-09-09 09:24:45 -0500

    rollup migrations

---
+++ priv/repo/migrations/20180908220145_initial_migration.exs
@@ -0,0 +1,26 @@
+defmodule Wikitrivia.Repo.Migrations.InitialMigration do
+  use Ecto.Migration
+
+  def change do
+    create table(:trivia_items) do
+      add :title, :string
+      add :description, :string
+      add :redacted_description, :string
+
+      timestamps()
+    end
+
+    create table(:questions) do
+      add :answer_id, references(:trivia_items, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create table(:questions_trivia_items, primary_key: false) do
+      add :question_id, references(:questions, on_delete: :nothing), primary_key: true
+      add :trivia_item_id, references(:trivia_items, on_delete: :nothing), primary_key: true
+    end
+
+    create index(:questions, [:answer_id])
+  end
+end
