85eb1d9919baa0e8d37e6c665fefe8fd33fee548
Dennis Heckman <denheck@gmail.com>
2018-09-21 21:36:54 -0500

    simplify db

--- priv/repo/migrations/20180908220145_initial_migration.exs
+++ priv/repo/migrations/20180908220145_initial_migration.exs
@@ -2,25 +2,12 @@ defmodule Wikitrivia.Repo.Migrations.InitialMigration do
   use Ecto.Migration
 
   def change do
-    create table(:answers) do
-      add :answer, :text
-
-      timestamps()
-    end
-
     create table(:questions) do
-      add :original, :text
-      add :redacted, :text
-      add :answer_id, references(:answers, on_delete: :nothing) # should this be :correct_answer_id
+      add :question, :text
+      add :correct_answer, :text
+      add :answers, {:array, :text}
 
       timestamps()
     end
-
-    create table(:question_answers, primary_key: false) do
-      add :question_id, references(:questions, on_delete: :nothing), primary_key: true
-      add :answer_id, references(:answers, on_delete: :nothing), primary_key: true
-    end
-
-    create index(:questions, [:answer_id])
   end
 end
