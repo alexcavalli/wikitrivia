3c87f8d45d94fd2ca64d405cbcce3da4afee6e9d
Dennis Heckman <denheck@gmail.com>
2018-09-21 21:28:09 -0500

    add JSON to mix.exs

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Wikitrivia.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:json, "~> 1.2"}
     ]
   end
 
